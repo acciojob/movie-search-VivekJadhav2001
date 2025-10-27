f0ff5d5413f26b1e2abf1a4d5febcc5a34b55320
Vivek Jadhav <vivek.jadhav200127@gmail.com>
2025-10-27 21:27:12 +0530

    Second Commit

--- src/components/MovieSearch.js
+++ src/components/MovieSearch.js
@@ -51,7 +51,7 @@ function MovieSearch() {
             <img src={movie.Poster !== "N/A" ? movie.Poster : "https://via.placeholder.com/150"} alt={movie.Title} className='w-full h-64 object-cover rounded-md' />
             <h2 className='mt-2 text-xl font-bold'>{movie.Title}</h2>
             <p className='text-gray-400'>{movie.Year}</p>
-          </div>
+          </div> 
         ))}
       </div>
     </div>
